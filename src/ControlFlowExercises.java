dcd83f4d71de07376876fc3edb6b8734be9d139a
bichtran <tranngocb@gmail.com>
2017-05-09 15:36:07 -0500

    Committing 1st exercice

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+/**
+ * Created by bichtran on 5/9/17.
+ */
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        int i = 5;
+        while (i <= 15) {
+
+            System.out.println("number: " + i);
+            i++;
+        }
+    }
+}
