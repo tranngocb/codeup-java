918938085178842937b53b5e5e0e7be33424e58b
bichtran <tranngocb@gmail.com>
2017-05-09 15:43:54 -0500

    Testing the connection

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,11 +4,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
+        /*Do Whil
+        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.*/
 
-            System.out.println("number: " + i);
-            i++;
-        }
+        int byTwo = 0;
+        do {
+            System.out.println("Incrieasing by 2: "+ byTwo);
+            byTwo += 2;
+        }while(byTwo <= 100);
+        System.out.println("End do while! ");
     }
 }
