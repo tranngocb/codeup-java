fdb9839efb22516fb00e11c7ba394404e584239c
bichtran <tranngocb@gmail.com>
2017-05-10 02:39:46 -0500

    Exercices on Wednesday 05/10/2017

--- src/ConsoleExercices.java
+++ src/ConsoleExercices.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -7,45 +8,49 @@ public class ConsoleExercices {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
         int nombreEntier;
-        System.out.println("Please enter your age: ");
+        PrintStream output = System.out;  //make a variable from System.out.println
+        output.println("Please enter your age: ");
         nombreEntier = scan.nextInt();
-        System.out.println("Your age is: " + nombreEntier);
+        output.println("Your age is: " + nombreEntier);
         scan.nextLine();    //To move the scanner to next line
 
         //String userInput;
-        System.out.println("Please enter 3 words: ");
+        output.println("Please enter 3 words: ");
         String userInput = scan.nextLine();
-        System.out.println("The full phrase is: " + userInput);
+        output.println("The full phrase is: " + userInput);
         String[] pieces = userInput.split("\\s+");
         String one = pieces[0];
         String two = pieces[1];
         String three = pieces[2];
-        System.out.println("1st: " + one + " Second: " + two + " Third: " + three);
+        output.println("1st: " + one + " Second: " + two + " Third: " + three);
         String sentence;
-        System.out.println("Please enter a sentence");
+        output.println("Please enter a sentence");
         //next() catch only the first word of the sentence
         sentence = scan.nextLine();
-        System.out.println("Sentence is: " + sentence);
+        output.println("Sentence is: " + sentence);
 //Bonus Accept decimal entries
         String userInputRoom;
-        System.out.println("Please enter the 3 numbers L, W and Height of the room");
+        output.println("Please enter the 3 numbers L, W and Height of the room");
         userInputRoom = scan.nextLine();
         pieces = userInputRoom.split("\\s+");
         String lng = pieces[0];
         String lat = pieces[1];
         String hei = pieces[2];
-        System.out.println("Length: "+ lng + "Width: " + lat);
+        output.println("Length: "+ lng + "Width: " + lat);
         //Convert to number
         //int lngNumber = Integer.parseInt(lng);
         //int latNumber = Integer.parseInt(lat);
         double lngvalue = Double.parseDouble(lng);
         double latvalue = Double.parseDouble(lat);
         double heiValue = Double.parseDouble(hei);
-        
+
         double area = lngvalue * latvalue;
         double perimter = (2 * lngvalue) + (2 * latvalue);
         double volume = lngvalue * latvalue * heiValue;
-        System.out.println("Area: " + area + " Perimeter: " + perimter + " Volume: "+ volume);
+        output.printf("Area: %.2f%n", area);
+        output.printf("Perimeter: %.2f\n" , perimter);
+
+               // + " Perimeter: " + perimter + " Volume: "+ volume);
 
 
 
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,14 +4,52 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        /*Do Whil
+        /*Do While
         Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.*/
 
         int byTwo = 0;
-        do {
-            System.out.println("Incrieasing by 2: "+ byTwo);
-            byTwo += 2;
-        }while(byTwo <= 100);
-        System.out.println("End do while! ");
+        int byFive = 0;
+        double startAt2 = 0;
+//        do {
+//            System.out.println("Increasing by 2: "+ byTwo);
+//            byTwo += 2;
+//        }while(byTwo <= 100);
+//        System.out.println("End do while! ");
+//
+        //Alter your loop to count backwards by 5's from 100 to -10.
+
+
+//        do {
+//            System.out.println("Decreasing by 5: " + byFive);
+//            byFive -= 5;
+//        }while (byFive >= -10);
+//
+//        //double startAt2 = 2;
+//        do {
+//            System.out.println("Squared number: " + startAt2);
+//            startAt2 = Math.pow(startAt2, 2);
+//        }while (startAt2 <1000000);
+// Using For loop instead
+       /* Create a For loop that runs so long as i is less than or equal to 15
+        Each loop iteration, output the current value of i, then increment i by one.  */
+        for (int i = 5;i<=15;i++) {
+            System.out.println("Start at 5 ; Increasing by 1: "+ i);
+        }
+        System.out.println("End of Start at 5.");
+
+        for (byTwo = 0; byTwo <= 100; byTwo += 2) {
+            System.out.println("Increasing by 2: "+ byTwo);
+        }
+        System.out.println("End of increasing by 2");
+
+        for (byFive = 100;byFive >= -10; byFive-= 5) {
+            System.out.println("Decreasing by 5: " + byFive);
+        }
+        System.out.println("End of Decreasing by 5");
+        for (startAt2 = 2;startAt2<1000000;startAt2 = Math.pow(startAt2, 2)) {
+            System.out.println("Squared number: " + startAt2);
+        }
+        System.out.println("End of Squared of 2");
+
     }
 }
