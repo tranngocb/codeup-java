85fae2ebc8dfcf03a5f51450d1c8e850ed42fb30
bichtran <tranngocb@gmail.com>
2017-05-11 10:56:36 -0500

    Commit on May 11 2017 at 11:00am

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+/**
+ * Created by bichtran on 5/11/17.
+ */
+public class StringExercise {
+    public static void main(String[] args) {
+//        StringBuffer str = new StringBuffer("We don't need no education");
+//        //int stringL = str.length();
+//        //System.out.println(stringL);
+//       str.replace( 17,(str.length()-1), "thought control");
+//        System.out.println(str);
+//        System.out.println(str.replace("no education","thought control haha "));
+
+        String message ="We don't need no education\n" + "We don't need no thought control\n"+"Check \"this\" out!, \"s inside of \"s!\n"
+                +"In windows, the main drive is usually C:\\\n\n"+ "I can do backslashes \\, double backslashes \\\\,\n"+"and the amazing triple backslash \\\\\\!";
+       //System.out.println(message.replace("education","thought control"));
+        //System.out.println("and the amazing triple backslash \\\\\\!");
+        System.out.println(message);
+    }
+}
